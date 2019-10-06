6f442e529c6695f90a9ae02cd74b913c81ebd4a8
Steve Lorenz <lorenz21@hawaii.edu>
2019-10-05 23:16:31 -0700

    Add header

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,25 @@ import React, { useEffect } from 'react';
 import { Provider, useDispatch } from 'react-redux';
 import createSagaMiddleware from 'redux-saga'
 import { createStore, applyMiddleware, compose } from 'redux';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
 import Chart from './components/Chart';
 
 import './styles/index.css';
 import rootReducer from './redux/rootReducer';
 import appSaga from './redux/saga';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+        main: '#f3f3f3',
+      },
+      secondary: {
+        main: '#ffd966',
+      },
+  },
+});
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -28,7 +41,7 @@ const InitApp = () => {
     );
 };
 
-const App = () => <Provider store={store}><InitApp /></Provider>;
+const App = () => <Provider store={store}><ThemeProvider theme={theme}><InitApp /></ThemeProvider></Provider>;
 
 
 export default App;
